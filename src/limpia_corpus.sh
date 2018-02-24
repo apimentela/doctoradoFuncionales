c620c580bcb3912dc61d375d48511a1bd39f8b34
Alejandro Pimentel <apimentenla@iingen.unam.mx>
2018-02-23 23:54:55 -0600

    Se modificó el programa de limpieza de corpus para tener buenas prácticas de documentación y de generalización del problema con opciones y toda la cosa. Aproveché para paralelizar y que corra más rapido si se le da el corpus fragmentado.
    Sería bueno hacer un programa de preprocesamiento que fragmente el corpus y mande llamar a este otro y posteriormente a la división de palabras y contextos según su función.

--- src/limpia_corpus.sh
+++ src/limpia_corpus.sh
@@ -1,31 +1,122 @@
 #!/bin/bash
 
-salida_en_lineas () {
-	entrada="$1"
-	salida="$2"
-	while read -ra linea; do	# la bandera -r es para que la diagonal invertida no sea caracter especial, la bandera -a es para que las palabras que se leen seinserten en un arrglo dentro de la varialbe
-		for palabra in "${linea[@]}"; do
-			echo "$palabra" >> "$salida"
-		done
-	done < "$entrada"
+##	limpia_corpus
+#	Este programa tiene el propósito de manipular un corpus de texto (inicialmente diseñado para ser de wikipedia)
+#	el programa tiene una serie de opciones para modificar la salida y las cosas que se eliminan del texto
+#	pero de manera estándar:
+#		- Quita todas las etiquetas de los artículos de wikipedia (como son extraídas de wikiextractor)
+#		- Elimina todo aquello que se encuentra entre paréntesis.
+#		- Elimina todos los signos de puntuación.
+#		- Convierte todas las letras en minúsculas.
+#		- Cambia todos los entes (separados por espacios) que contengan algún número por la palabra clave DIGITO.
+#		- Elimina las líneas vacías.
+##	DEPENDENCIAS
+#	- parallel
+
+nombre_programa=$0
+
+function usage {
+##	Uso
+#	Esta función muestra en pantalla el modo de empleo de este programa
+
+    echo "Uso: ./$nombre_programa [OPCIÓN]... [-o SUFIJO_SALIDA] ARCHIVO_ENTRADA..."
+    echo "	-o SUFIJO_SALIDA, --output=SUFIJO_SALIDA
+		Establece el sufijo para los diferentes archivos de salida,
+		si se omite se usará como salida: salida_limpia_corpus"
+    echo "	-w, --wikipedia
+		Activa la opción de eliminación de las etiquetas xml
+		de los artículos de wikipedia" #TODO: Esta opción por ahora está activada siempre, hace falta implementarla.
+    echo "	-p, --parentesis, --parenthesis
+		Desactiva la eliminación de los objetos entre parentesis" #TODO
+	echo "	-s, --signos, --simbolos, --punct, --punctuation, --puntuacion
+		Desactiva la eliminación de los signos de puntuación" #TODO
+	echo "	-M, --mayus
+		Desactiva la transformación a minúsculas" #TODO
+	echo "	-m, --minus
+		Activa la transformación a minúsculas, activado por defecto" #TODO
+    echo "	-h, --help, --ayuda
+		Muestra la ayuda"
+	echo "	-n,--num
+		Desactiva la eliminación de los números" #TODO
+	echo "	-d REMPLAZO, --digito=REMPLAZO
+		Recibe como parámetro la etiqueta con la que se van a reemplazar
+		todos los números, por defecto esta opción está activada con la etiqueta DIGITO" #TODO
+    echo "	-e, --empty
+		Desactiva la eliminación de las líneas vacías" #TODO
 }
-export -f salida_en_lineas
 
-entrada="$1"
-salida="$2"
-#salida="$entrada"
+# Transform long options to short ones
+for arg in "$@"; do
+  shift
+  case "$arg" in
+	"--output") set -- "$@" "-o" ;; #FIXME: esta opción recibe un parámetro, hay que convertirla para que acepte el símbolo =
+	"--wikipedia") set -- "$@" "-w" ;;
+	"--parentesis") set -- "$@" "-p" ;;
+	"--parenthesis") set -- "$@" "-p" ;;
+	"--signos") set -- "$@" "-s" ;;
+	"--simbolos") set -- "$@" "-s" ;;
+	"--punct") set -- "$@" "-s" ;;
+	"--punctuation") set -- "$@" "-s" ;;
+	"--puntuacion") set -- "$@" "-s" ;;
+	"--mayus") set -- "$@" "-M" ;;
+	"--minus") set -- "$@" "-m" ;;
+	"--help") set -- "$@" "-h" ;;
+	"--ayuda") set -- "$@" "-h" ;;
+	"--num") set -- "$@" "-n" ;;
+	"--digito") set -- "$@" "-d" ;; #FIXME: esta opción recibe un parámetro, hay que convertirla para que acepte el símbolo =
+	"--empty") set -- "$@" "-e" ;;
+	"--"*) echo "Opción desconocida: $arg" >&2 ; usage ; exit 1;; #TODO: Hay que ver si esto funciona, sobre todo al usar tan solo -- que es para terminar opciones
+    *)        set -- "$@" "$arg"
+  esac
+done
 
-if [ -d "$entrada" ]; then
-	#~ for arhivo in "$entrada", do
-		#~ salida_en_lineas "$archivo" "$salida"
-	#~ done
-	find "$entrada" -type f -exec cat {} >> "$entrada" \;
-fi
+# Default behavior
+export flag_wiki=false
+export flag_parentesis=false
+export flag_punct=false
+export flag_mayus=false
+export flag_minus=true
+export flag_num=false
+export etiqueta_DIGITO="DIGITO"
+export flag_empty=false
+
+# Parse short options
+OPTIND=1
+while getopts "o:wpsMmhnd:e" opt
+do
+  case "$opt" in
+	"o") salida="$OPTARG" ;;
+	"w") flag_wiki=true ;;
+	"p") flag_parentesis=true ;;
+	"s") flag_punct=true ;;
+	"M") flag_mayus=true ; flag_minus=false ;;
+	"m") flag_minus=true ; flag_mayus=false ;;
+	"h") usage; exit 0 ;;
+	"n") flag_num=true ;;
+	"d") etiqueta_DIGITO="$OPTARG" ;;
+	"e") flag_empty=true ;;
+	"?") echo "Opción desconocida: -$OPTARG" >&2 ; usage ; exit 1;;
+  esac
+done
+shift $(expr $OPTIND - 1) # remove options from positional parameters
+
+# Opción final de la salida
+: ${salida:="salida_limpia_corpus"} # Esto es una asignación por defecto de un valor, si no se ha establecido el valor de salida, se usa el segundo valor (el de la primera entrada)
+
+function main() {
+	cat "$1" \
+	| if [[ $flag_wiki == true ]]; then sed -e 's|^</*doc.*$||g'; else cat; fi \
+	| if [[ $flag_parentesis == false ]]; then sed -e 's|([^)]*)||g'; else cat; fi \
+	| if [[ $flag_punct == false ]]; then sed -e 's|[[:punct:]]||g'; else cat; fi \
+	| if [[ $flag_minus == true ]]; then perl -C -ne 'print lc'; else cat; fi \
+	| if [[ $flag_num == false ]]; then perl -C -pe 's/[^ ]*\d[^ ]*/'"$etiqueta_DIGITO"'/g'; else cat; fi
+}
+export -f main
 
 #FIXME: En algunos puntos del corpus hay espacios que son identicos a los espacios per no son espacios, hay que limpiar eso, tampoco son capturados por [:space:]
-sed -e 's|^</*doc.*$||g' -e 's|([^)]*)||g' -e 's|[[:punct:]]||g' "$entrada" | tr -s [:space:] > "$salida"
-#perl -C -ne 'print lc' "$salida" | perl -C -pe 's/\d+([^ ]\d+)*/DIGITO/g' > "${salida}_minusculas"
-perl -C -ne 'print lc' "$salida" | perl -C -pe 's/[^ ]*\d[^ ]*/DIGITO/g' > "${salida}_minusculas"
-cat "${salida}_minusculas" | tr [:space:] "\n" | tr -s [:space:] > "${salida}_oneperline"
-sort "${salida}_oneperline" | uniq -c | sort -rn > "${salida}_freqs"
-awk '{printf "%f %s\n", $1/length($2),$2}' "${salida}_freqs" | sort -rn > "${salida}_functionScores"
+parallel --env _ --linebuffer main ::: $@ \
+| if [[ $flag_empty == false ]]; then tr -s [:space:]; else cat; fi > "$salida"
+
+#~ cat "${salida}_minusculas" | tr [:space:] "\n" | tr -s [:space:] > "${salida}_oneperline"
+#~ sort "${salida}_oneperline" | uniq -c | sort -rn > "${salida}_freqs"
+#~ awk '{printf "%f %s\n", $1/length($2),$2}' "${salida}_freqs" | sort -rn > "${salida}_functionScores"
