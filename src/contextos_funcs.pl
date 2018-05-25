4073817deeb6b9a327f76f7d8a44c8754c851e8c
Alejandro Pimentel <apimentenla@iingen.unam.mx>
2018-05-24 20:30:20 -0500

    además de la función multifuncs.pl, también la de contextos_funcs.pl tenía esa misma expresión, aún no las prueba pero ya cambié ambas, junto con las funciones de los vectores (que los mandan llamar) y que además usaban como separador la coma, ahora la cambié por dos puntos (:)

--- src/contextos_funcs.pl
+++ src/contextos_funcs.pl
@@ -14,8 +14,11 @@ my $palabras_funcionales = read_file($archivo_palabras);
 chomp($palabras_funcionales);	# Es mas facil usar chomp que: substr($palabras_funcionales,0,-1); Para quitar el último salto de linea
 $palabras_funcionales =~ s/\n/|/g;
 #~ $unaOmas_funcionales = '(?: ?\b(?:' . $palabras_funcionales . ')\b ?)+';	# Esta expresion es para una o más funcionales, pero si la funcional es multipalabra ya, entonces solo queresmos una ya
-$funcionales = '(?: ?\b(?:' . $palabras_funcionales . ')\b ?)';
+
+#~ $funcionales = '(?: ?\b(?:' . $palabras_funcionales . ')\b ?)';	# Esto seguramente tendrá el mismo problema de no detectar comas por la \b
+$funcionales = '(?: (?:' . $palabras_funcionales . ') )';
 $expresion_palabras = '(\S*)(' . $funcionales . ')(?=(\S*))';	# Esta expresión devuelve contextos de palabras funcionales rodeados de una palabra no funcional. No se si sea mejor que busque todas las palabras no funcionales juntas, quizá no haga falta, y no se si la siguiente expresión sea más productiva de cualquier forma.
+
 #~ $expresion_palabras = '(' . $unaOmas_funcionales . ')( .+? )(?=(' . $unaOmas_funcionales . '))'; # Esta expresión regresa todos los contextos de palabras no funcionales rodeados de todoas las palabras funcionales juntas que encuentra al rededor
 #$expresion_palabras = '((?: ?\b(?:' . $palabras_funcionales . ')\b ?))( .+? )(?=((?: ?\b(?:' . $palabras_funcionales . ')\b ?)))'; # ERROR. Esta expresión toma solo UNA palabra funciona, quiza sea suficiente para las primeras pruebas # ERROR; esto no funcionó, regresa dentro de los contextos palabras funcionales.
 
@@ -23,7 +26,7 @@ open(INPUT,"<$archivo_contenido") or die "No se pudo abrir el archivo, $!";
 my @coincidencias;
 while(<INPUT>){
 	while ($_ =~ /$expresion_palabras/g){
-		chomp(my $salida = "$1,$2,$3");
+		chomp(my $salida = "$1:$2:$3");
 		print "$salida\n";	
 	}	
 }
