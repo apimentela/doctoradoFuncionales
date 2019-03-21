3dc0bb4d6df6b81f12fcb28fd87383affdfed6a8
Alejandro Pimentel <apimentenla@iingen.unam.mx>
2019-03-20 21:58:26 -0600

    Correcciones

--- src/extrae_pares.pl
+++ src/extrae_pares.pl
@@ -12,18 +12,18 @@ my $archivo_entrada = $ARGV[1];
 my $lista_funcs_1 = 'la|el|los|las|del|su|al|un|se|the|san|una|lo|n|sus|dos';
 my $lista_funcs_2 = 'de|y|en|a|con|es|por|para|hasta|of|fue|o|que|durante|sobre|contra|como';
 
-my $expresion_1 = '\b(.+)(?:' . $lista_funcs_1 . ' )? (' . $palabra_estimulo . ') (?:\S+) \1 (?:' . $lista_funcs_1 . ' )?(\S+)\b';
-my $expresion_2 = '\b(.+)(?:' . $lista_funcs_1 . ' )? (\S+) (?:\S+) \1 (?:' . $lista_funcs_1 . ' )?(' . $palabra_estimulo . ')\b';
+my $expresion_1 = '\b(.+)(?:(?:' . $lista_funcs_1 . ') )? (' . $palabra_estimulo . ') (?:\S+) \1 (?:(?:' . $lista_funcs_1 . ') )?(\S+)\b';
+my $expresion_2 = '\b(.+)(?:(?:' . $lista_funcs_1 . ') )? (\S+) (?:\S+) \1 (?:(?:' . $lista_funcs_1 . ') )?(' . $palabra_estimulo . ')\b';
 
 open(INPUT,"<$archivo_entrada") or die "No se pudo abrir el archivo, $!";
 while(<INPUT>){
 	while ($_ =~ /$expresion_1/g){
-		#~ if ( $1 eq $2 ) { next; }
-		print "$2 $3";	
+		if ( $3 =~ /'\b(?:' . $lista_funcs_1 . '|' . $lista_funcs_2 . ')\b'/ ) { next; }
+		print "$2 $3\n";	
 	}
 	while ($_ =~ /$expresion_2/g){
-		#~ if ( $1 eq $2 ) { next; }
-		print "$3 $2";	
+		if ( $2 =~ /'\b(?:' . $lista_funcs_1 . '|' . $lista_funcs_2 . ')\b'/ ) { next; }
+		print "$3 $2\n";	
 	}
 }
 close INPUT;
